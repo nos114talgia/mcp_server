fda87417e4fdd29d29473737fcda8ce3f92c410f
Bowen Zheng <zbw1469902638@gmail.com>
2026-03-30 20:58:04 +0800

    add a new server.h

---
+++ src/server/Server.h
@@ -0,0 +1,93 @@
+#ifndef MCP_SERVER_SERVER_H
+#define MCP_SERVER_SERVER_H 
+
+#include <memory>
+#include <mutex>
+#include <string>
+#include <unordered_map>
+#include <queue>
+#include <thread>
+#include <condition_variable>
+#include "ITransport.h"
+#include "json.hpp"
+
+using json = nlohmann::json;
+
+#define MAX_PARSE_ERRORS 50
+
+namespace vx::mcp {
+    enum Capabilities {
+        RESOURCE = 0 << 1,
+        TOOLS = 0 << 2,
+        PROMPTS = 0 << 3,
+    };
+
+    class Server {
+    public:
+        Server();
+        ~Server();
+
+        Server(const Server&) = delete;
+        Server(Server&&) = delete;
+        Server& operator=(const Server&) = delete;
+        Server& operator=(Server&&) = delete;
+
+        bool Connect(const std::shared_ptr<ITransport>& transport);
+        bool ConnectAsync(const std::shared_ptr<ITransport>& transport);
+
+        void Stop();
+        void StopAsync();
+
+        inline bool IsValid() {return transport_ != nullptr;}
+        inline void VerboseLevel(int level) {verboseLevel_ = level;}
+        inline void Name(const std::string& name) {name_ = name;}
+        bool OverrideCallback(const std::string& method, std::function<json(const json&)> function);
+        void SendNotification(const std::string& pluginName, const char* notification);
+    private:
+        void WriterLoop();
+        json HandleRequest(const json& request);
+
+        json InitializeCmd(const json& request);
+        json PingCmd(const json& request);
+        json NotificationInitializedCmd(const json& request);
+        json ToolsListCmd(const json& request);
+        json ToolsCallCmd(const json& request);
+
+        json ResourcesListCmd(const json& request);
+        json ResourcesReadCmd(const json& request);
+        json ResourcesSubscribeCmd(const json& request);
+        json ResourcesUnsubscribeCmd(const json& request);
+        json PromptsListCmd(const json& request);
+        json PromptsGetCmd(const json& request);
+        json LoggingSetLevelCmd(const json& request);
+        json CompletionCompleteCmd(const json& request);
+        json RootsListCmd(const json& request);
+
+        json NotificationCancelledCmd(const json& request);
+        json NotificationProgressCmd(const json& request);
+        json NotificationRootsListChangedCmd(const json& request);
+        json NotificationResourcesListChangedCmd(const json& request);
+        json NotificationResourcesUpdatedCmd(const json& request);
+        json NotificationPromptsListChangedCmd(const json& request);
+        json NotificationToolsListChangedCmd(const json& request);
+        json NotificationMessageCmd(const json& request);
+
+    private:
+        std::unordered_map<std::string, std::function<json(const json&)>> functionMap;
+        bool isStopping_ = false;
+        int verboseLevel_ = 0;
+        int parseErrores_ = 0;
+        std::string name_ = "mcp-server";
+
+        std::unique_ptr<ITransport> transport_;
+        std::queue<std::string> notification_queue_;
+        std::mutex output_mutex_;
+        std::condition_variable queue_cv_;
+        std::thread writer_thread_;
+        std::thread reader_thread_;
+        std::atomic<bool> writer_running_ = false;
+        std::atomic<bool> reader_running_ = false;
+    };
+}
+
+#endif
